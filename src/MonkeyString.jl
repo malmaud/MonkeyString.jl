a78d1de199780cead2fbac8517173ec7639f0859
Jon Malmaud <malmaud@gmail.com>
2015-07-30 08:33:32 -0400

    .

--- src/MonkeyString.jl
+++ src/MonkeyString.jl
@@ -1,7 +1,8 @@
 module M
 
-import Base: show, +, getindex
-export MString
+import Base: show, +, getindex, convert, length, ==
+export @m_str, MString
+
 
 type MString <: AbstractString
     kind::DataType
@@ -18,14 +19,32 @@ type MString <: AbstractString
     left::MString
     right::MString
 
-    function MString(s::AbstractString)
-        m = new()
-        m.kind = FixedString
-        m.data = s.data
-        m
+    # Extensible
+    pointer::Int
+
+    function MString(s::ByteString)
+        get(ATOMS, hash(s.data)) do
+            m = new()
+            m.kind = FixedString
+            m.data = s.data
+            m
+        end
     end
 
     MString() = new()
+end
+
+MString(s::AbstractString) = MString(utf8(s))
+
+const ATOMS = Dict{UInt64, MString}()
+
+
+macro m_str(s)
+    get!(ATOMS, hash(s.data)) do
+        m = MString(s)
+        m.kind = AtomString
+        m
+    end
 end
 
 abstract BaseString
@@ -35,18 +54,28 @@ abstract DependentString <: LinearString
 abstract FlatString <: LinearString
 abstract FixedString <: FlatString
 abstract ExtensibleString <: FlatString
+abstract AtomString <: FixedString
 
 show(io::IO, m::MString) = show(io, m, m.kind)
 
-show(io, m, ::Type{FixedString}) = show(io, utf8(m))
+show{T<:FlatString}(io, m, ::Type{T}) = show(io, utf8(m))
+
 function show(io, m, ::Type{DependentString})
     print(io, "Dependent ($(m.offset), $(m.length)) of ")
     show(io, m.backing)
 end
 
-function Base.convert(::Type{UTF8String}, m::MString)
+convert(::Type{UTF8String}, m::MString) = convert(UTF8String, m, m.kind)
+
+function convert{T<:BaseString}(::Type{UTF8String}, m, ::Type{T})
     ensureflat(m)
-    utf8(m.data)
+    convert(UTF8String, m)
+end
+
+convert{T<:FixedString}(::Type{UTF8String}, m, ::Type{T}) = utf8(m.data)
+
+function convert(::Type{UTF8String}, m, ::Type{ExtensibleString})
+    utf8(pointer(m.data), m.pointer)
 end
 
 ensureflat(m::MString) = ensureflat(m, m.kind)
@@ -54,17 +83,51 @@ ensureflat(m::MString) = ensureflat(m, m.kind)
 ensureflat{T<:FlatString}(m, ::Type{T}) = m
 
 function ensureflat(m, ::Type{DependentString})
-    ensureflat(m.backing)
-    m.data = m.backing.data[(m.offset+1):(m.offset+m.length)]
+    backing, offset, length = m.backing, m.offset, m.length
+    while backing.kind == DependentString
+        if length > backing.length
+            error("Too long")
+        end
+        offset += backing.offset
+        backing = backing.backing
+    end
+    ensureflat(backing)
+    m.data = backing.data[(offset+1):(offset+length)]
     m.kind = FixedString
     m
 end
 
+function buffersize(n)
+    exp = floor(Int, log2(n)) + 1
+    2^exp
+end
+
+length(m::MString) = length(m, m.kind)::Int
+
+length(m::MString, ::Type{RopeString}) = length(m.left) + length(m.right)
+length{T<:FixedString}(m::MString, ::Type{T}) = length(m.data)
+length(m::MString, ::Type{ExtensibleString}) = m.pointer
+length(m::MString, ::Type{DependentString}) = m.length
+
+
 function ensureflat(m, ::Type{RopeString})
     ensureflat(m.left)
     ensureflat(m.right)
-    m.data = vcat(m.left.data, m.right.data)
-    m.kind = FixedString
+    ll = length(m.left)
+    lr = length(m.right)
+    outlength = ll + lr
+    if m.left.kind == ExtensibleString && length(m.left.data) > outlength
+        m.data = m.left.data
+        unsafe_copy!(m.data, ll+1, m.right.data, 1, lr)
+        m.pointer = outlength
+    else
+        m.data = Vector{UInt8}(buffersize(outlength))
+        info("Resized to $(length(m.data))")
+        unsafe_copy!(m.data, 1, m.left.data, 1, ll)
+        unsafe_copy!(m.data, ll+1, m.right.data, 1, lr)
+        m.pointer = outlength
+    end
+    m.kind = ExtensibleString
     m
 end
 
@@ -74,6 +137,9 @@ function +(left::MString, right::MString)
     m.left, m.right = left, right
     m
 end
+
++(left::MString, right) = left + MString(right)
++(left, right::MString) = MString(left) + right
 
 function show(io, m, ::Type{RopeString})
     print(io, "(")
@@ -93,5 +159,16 @@ function getindex(backing::MString, r::UnitRange{Int})
     m
 end
 
+asarray{T<:FixedString}(m, ::Type{T}) = m.data
+asarray(m, ::Type{ExtensibleString}) = slice(m.data, 1:m.pointer)
+
+==(left::MString, right::MString) = ==(left, right, left.kind, right.kind)
+==(left::MString, right::AbstractString) = left==MString(right)
+==(left::AbstractString, right::MString) = MString(left)==right
+
+==(left, right, ::Type{AtomString}, ::Type{AtomString}) = left===right
+function =={T<:FlatString, G<:FlatString}(left, right, ::Type{T}, ::Type{G})
+    asarray(left, T) == asarray(right, G)
+end
 
 end
