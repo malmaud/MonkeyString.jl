1658ab40e8dbe43c47f6e9c7fea2632f28f1cc5b
Jon Malmaud <malmaud@gmail.com>
2015-08-02 05:59:18 -0400

    Added atoms

--- src/MonkeyString.jl
+++ src/MonkeyString.jl
@@ -3,6 +3,7 @@ module M
 import Base: show, +, getindex, convert, length, ==
 export @m_str, MString
 
+const INLINE_SIZE=8
 
 type MString <: AbstractString
     kind::DataType
@@ -22,7 +23,24 @@ type MString <: AbstractString
     # Extensible
     pointer::Int
 
+    # Inline
+    inline::NTuple{INLINE_SIZE, UInt8}
+
+    function MString{N}(s::NTuple{N, UInt8})
+        N > INLINE_SIZE && error("Tuple too big ($N vs $INLINE_SIZE)")
+        m = new()
+        m.kind = InlineString
+        m.length = N
+        d = zeros(UInt8, INLINE_SIZE)
+        d[1:N] = [s...]
+        m.inline = (d...)
+        m
+    end
+
     function MString(s::ByteString)
+        if length(s) ≤ INLINE_SIZE
+            return MString((s.data...))
+        end
         get(ATOMS, hash(s.data)) do
             m = new()
             m.kind = FixedString
@@ -36,13 +54,18 @@ end
 
 MString(s::AbstractString) = MString(utf8(s))
 
+
 const ATOMS = Dict{UInt64, MString}()
 
 
 macro m_str(s)
     get!(ATOMS, hash(s.data)) do
         m = MString(s)
-        m.kind = AtomString
+        if m.kind == InlineString
+            m.kind = InlineAtomString
+        else
+            m.kind = AtomString
+        end
         m
     end
 end
@@ -55,6 +78,9 @@ abstract FlatString <: LinearString
 abstract FixedString <: FlatString
 abstract ExtensibleString <: FlatString
 abstract AtomString <: FixedString
+abstract InlineString <: FixedString
+abstract InlineAtomString <: InlineString
+typealias IsAtom Union{InlineAtomString, AtomString}
 
 show(io::IO, m::MString) = show(io, m, m.kind)
 
@@ -72,11 +98,8 @@ function convert{T<:BaseString}(::Type{UTF8String}, m, ::Type{T})
     convert(UTF8String, m)
 end
 
-convert{T<:FixedString}(::Type{UTF8String}, m, ::Type{T}) = utf8(m.data)
+convert{T<:FixedString}(::Type{UTF8String}, m, ::Type{T}) = utf8(asarray(m, T))
 
-function convert(::Type{UTF8String}, m, ::Type{ExtensibleString})
-    utf8(pointer(m.data), m.pointer)
-end
 
 ensureflat(m::MString) = ensureflat(m, m.kind)
 
@@ -108,7 +131,8 @@ length(m::MString, ::Type{RopeString}) = length(m.left) + length(m.right)
 length{T<:FixedString}(m::MString, ::Type{T}) = length(m.data)
 length(m::MString, ::Type{ExtensibleString}) = m.pointer
 length(m::MString, ::Type{DependentString}) = m.length
-
+length{T<:InlineString}(m::MString, ::Type{T}) = m.length
+length(m::MString, ::Type{RopeString}) = m.length + length(m.right)
 
 function ensureflat(m, ::Type{RopeString})
     ensureflat(m.left)
@@ -135,6 +159,7 @@ function +(left::MString, right::MString)
     m = MString()
     m.kind = RopeString
     m.left, m.right = left, right
+    m.length = length(m.left)
     m
 end
 
@@ -142,7 +167,7 @@ end
 +(left, right::MString) = MString(left) + right
 
 function show(io, m, ::Type{RopeString})
-    print(io, "(")
+    print(io, "Rope(")
     show(io, m.left)
     print(io, ", ")
     show(io, m.right)
@@ -159,16 +184,49 @@ function getindex(backing::MString, r::UnitRange{Int})
     m
 end
 
+getindex(m::MString, idx::Int) = getindex(m, m.kind, idx)::Char
+getindex{T<:FlatString}(m, ::Type{T}, idx) = Char(m.data[idx])
+getindex{T<:InlineString}(m, ::Type{T}, idx) = Char(m.inline[idx])
+function getindex{T<:RopeString}(m, ::Type{T}, idx)
+    if idx ≤ m.length
+        m.left[idx]
+    else
+        m.right[idx - m.length]
+    end
+end
+function getindex{T<:DependentString}(m, ::Type{T}, idx)
+    m.backing[idx + m.offset]
+end
+
+
 asarray{T<:FixedString}(m, ::Type{T}) = m.data
 asarray(m, ::Type{ExtensibleString}) = slice(m.data, 1:m.pointer)
+asarray{T<:InlineString}(m, ::Type{T}) = [m.inline[1:m.length]...]
 
 ==(left::MString, right::MString) = ==(left, right, left.kind, right.kind)
 ==(left::MString, right::AbstractString) = left==MString(right)
 ==(left::AbstractString, right::MString) = MString(left)==right
 
-==(left, right, ::Type{AtomString}, ::Type{AtomString}) = left===right
+=={T<:IsAtom, G<:IsAtom}(left, right, ::Type{T}, ::Type{G}) = left===right
+=={T<:InlineString, G<:InlineString}(left, right, ::Type{T}, ::Type{G}) = left.inline == right.inline && left.length == right.length
+
+function =={T<:FlatString, G<:InlineString}(left, right, ::Type{G}, ::Type{T})
+    N = length(left, InlineString)
+    N == length(right, T) || return false
+    for i=1:N
+        left.inline[i] == right.data[i] || return false
+    end
+    true
+end
+
+=={T<:FlatString, G<:InlineString}(left, right, ::Type{T}, ::Type{G}) = right==left
+
 function =={T<:FlatString, G<:FlatString}(left, right, ::Type{T}, ::Type{G})
     asarray(left, T) == asarray(right, G)
+end
+
+function Base.match(r::Regex, m::MString, offset=0)
+    match(r, utf8(m), offset)
 end
 
 end
